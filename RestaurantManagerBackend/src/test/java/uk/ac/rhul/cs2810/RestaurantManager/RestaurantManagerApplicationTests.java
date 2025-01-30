32b1d350673a88115b3f141dd0c61097da6e057e
Dawson Nadya (2023) ZMAC261 <nadya.dawson.2023@live.rhul.ac.uk>
2025-01-30 17:44:58 +0000

    #18 initialised Maven Project

---
+++ RestaurantManagerBackend/src/test/java/uk/ac/rhul/cs2810/RestaurantManager/RestaurantManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2810.RestaurantManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
