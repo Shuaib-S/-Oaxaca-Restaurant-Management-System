02e079235b250b5fa38c879e0d547ed5c4f52adb
Marcus Haynes <marcus.haynes.2023@live.rhul.ac.uk>
2025-02-13 20:13:16 +0000

    #63 updated controller with ResponseEntity

--- src/main/java/uk/ac/rhul/cs2810/RestaurantManager/controller/OrderController.java
+++ src/main/java/uk/ac/rhul/cs2810/RestaurantManager/controller/OrderController.java
@@ -2,10 +2,14 @@ package uk.ac.rhul.cs2810.RestaurantManager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import uk.ac.rhul.cs2810.RestaurantManager.repository.OrderRepository;
+import uk.ac.rhul.cs2810.RestaurantManager.model.Order;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -16,6 +20,12 @@ public class OrderController {
   @Autowired
   public OrderController(OrderRepository orderRepository) {
     this.orderRepository = orderRepository;
+  }
+
+  @PostMapping
+  public ResponseEntity<Order> addOrder(@RequestBody Order order) { // update this with post data
+    orderRepository.save(order);
+    return ResponseEntity.ok(order);
   }
 
 }
