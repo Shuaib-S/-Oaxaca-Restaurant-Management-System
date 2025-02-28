bd015ed40b072b762b9d0dcc0b9da4b8c34b0df1
Nadya_Dawson <zmac261@live.rhul.ac.uk>
2025-02-28 11:06:31 +0000

    added controller file for login

---
+++ src/main/java/uk/ac/rhul/cs2810/RestaurantManager/controller/LoginController.java
@@ -0,0 +1,39 @@
+package uk.ac.rhul.cs2810.RestaurantManager.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import uk.ac.rhul.cs2810.RestaurantManager.repository.OrderRepository;
+import uk.ac.rhul.cs2810.RestaurantManager.repository.LoginRepository;
+import uk.ac.rhul.cs2810.RestaurantManager.model.Order;
+import uk.ac.rhul.cs2810.RestaurantManager.model.Item;
+
+
+@RestController
+@RequestMapping("/api/login")
+public class LoginController {
+
+
+  private final LoginRepository loginRepository;
+
+  @Autowired
+  public LoginController(LoginRepository loginRepository) {
+    this.loginRepository = loginRepository;
+  }
+
+  @PostMapping
+  public ResponseEntity<Integer> checkLogin(@RequestBody Object login){
+    System.out.println(login);
+    return ResponseEntity.ok(1);
+  }
+
+}
