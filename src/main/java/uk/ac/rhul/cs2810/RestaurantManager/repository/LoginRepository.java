06d3d1d6a9a6ea79b7356706ae4fde4a217de7b7
Nadya_Dawson <zmac261@live.rhul.ac.uk>
2025-02-28 10:52:45 +0000

    added a LoginRepository to be used with a controller

---
+++ src/main/java/uk/ac/rhul/cs2810/RestaurantManager/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package uk.ac.rhul.cs2810.RestaurantManager.repository;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import uk.ac.rhul.cs2810.model.Login;
+
+public interface LoginRepository extends CrudRepository<Login, Integer> {
+
+}
