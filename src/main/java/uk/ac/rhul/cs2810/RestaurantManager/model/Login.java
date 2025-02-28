593e8d8faed2a841edc62fc0d98305ee020f6a7a
Nadya_Dawson <zmac261@live.rhul.ac.uk>
2025-02-28 10:48:05 +0000

    added a login model file

---
+++ src/main/java/uk/ac/rhul/cs2810/RestaurantManager/model/Login.java
@@ -0,0 +1,57 @@
+package uk.ac.rhul.cs2810.RestaurantManager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+/**
+ * A class that handles the logins of staff members in the restaurant.
+ */
+@Entity
+@Table(name = "logins")
+public class Login {
+    @Id
+    int id;
+
+    private String username;
+    private String password;
+
+      /**
+   * 
+   * Constructor that adds the id, username and password of the login.
+   * 
+   * @param username The username of a staff member.
+   * @param password The password of a staff member.
+   */
+    public Login(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
