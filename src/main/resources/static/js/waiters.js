8ad2517b0de85d3ca51f76c6803db274f4088316
AngeloG503 <126010157+AngeloG503@users.noreply.github.com>
2025-02-28 11:18:09 +0000

    Linked the js file to the html file, and fixed the function as I forgot to querySelector ALL.

--- src/main/resources/static/js/waiters.js
+++ src/main/resources/static/js/waiters.js
@@ -1,5 +1,5 @@
 
-document.querySelector('.table').forEach(table => {
+document.querySelectorAll('.table').forEach(table => {
     table.addEventListener('click', function() {
         window.location.href = `/table.html?tableId=${this.id}`;
     });
