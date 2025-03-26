e2deb66a45bcc6a9af74998e2d76d0113ca36ffb
Angelo Georgiou <zmac593@live.rhul.ac.uk>
2025-03-26 22:16:39 +0000

    Fixed conflicts

--- src/main/java/uk/ac/rhul/cs2810/RestaurantManager/repository/NotificationRepository.java
+++ src/main/java/uk/ac/rhul/cs2810/RestaurantManager/repository/NotificationRepository.java
@@ -1,4 +1,14 @@
 package uk.ac.rhul.cs2810.RestaurantManager.repository;
 
-public class NotificationRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import uk.ac.rhul.cs2810.RestaurantManager.model.Notification;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends CrudRepository<Notification, Long> {
+  List<Notification> findByStaffTypeAndIsDoneOrderByCreatedAtDesc(String staffType, boolean isDone);
+
+  long countByStaffTypeAndIsDone(String staffType, boolean isDone);
 }
